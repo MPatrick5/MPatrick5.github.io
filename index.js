8d7d84a8f2e8b8625fbf1c6c12828acd7961643c
MPatrick5 <yarin.baz109@gmail.com>
2023-10-28 05:43:14 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("your mom")
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Ask a question
+rl.question('What is your name? ', (answer) => {
+  console.log(`Hello, ${answer}!`);
+  rl.close(); // Close the interface
+});
